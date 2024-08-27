28eeba98770e2f6253f71e6bfb4155ffe765c369
mmheg <moustafa.hegazy@ejust.edu.eg>
2024-08-28 00:51:31 +0300

    added courses to their prespective table

---
+++ insertCourses.sql
@@ -0,0 +1,6 @@
+INSERT INTO Courses (course_name, course_description) VALUES
+('Mathematics', 'An introductory course to Mathematics'),
+('Computer Science', 'Fundamentals of programming, algorithms, and data structures'),
+('Physics', 'Basic principles of physics including mechanics and thermodynamics'),
+('History', 'A survey of world history from ancient to modern times'),
+('Biology', 'Introduction to biological sciences, covering cell biology, genetics, and ecology');
