a3975f791ffb19c8a2d2a9f209fd63f25594a296
mmheg <moustafa.hegazy@ejust.edu.eg>
2024-08-28 00:53:08 +0300

    added instuctors to the database

---
+++ insertInstructors.sql
@@ -0,0 +1,4 @@
+INSERT INTO Instructors (first_name, last_name, email) VALUES
+('Ahmed', 'Ezab', 'ahmed.ezab@gmail.com'),
+('Mona', 'Shawky', 'mona.shawky@gmail.com'),
+('Tarek', 'Adel', 'tarek.adel@gmail.com');
