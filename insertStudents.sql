026f3833976ce0cae3ea39f348d5d8ea0e9b01fb
mmheg <moustafa.hegazy@ejust.edu.eg>
2024-08-28 00:54:01 +0300

    added student's data to the database

---
+++ insertStudents.sql
@@ -0,0 +1,11 @@
+INSERT INTO Students (first_name, last_name, email, date_of_birth) VALUES
+('Ahmed', 'Mostafa', 'ahmed.mostafa@example.com', '2000-05-12'),
+('Fatma', 'Hassan', 'fatma.hassan@example.com', '2001-08-23'),
+('Omar', 'El-Sayed', 'omar.elsayed@example.com', '2002-11-30'),
+('Sara', 'Ali', 'sara.ali@example.com', '2003-07-19'),
+('Mohamed', 'Khaled', 'mohamed.khaled@example.com', '2004-02-15'),
+('Yasmin', 'Ibrahim', 'yasmin.ibrahim@example.com', '2000-09-08'),
+('Khaled', 'Abdelrahman', 'khaled.abdelrahman@example.com', '2001-03-25'),
+('Aya', 'Gamal', 'aya.gamal@example.com', '2002-06-14'),
+('Hassan', 'Mahmoud', 'hassan.mahmoud@example.com', '2005-12-21'),
+('Mona', 'Tarek', 'mona.tarek@example.com', '2004-10-10');
